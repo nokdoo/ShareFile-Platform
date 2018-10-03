af7f8b8e0f6d579b4cedfae38586839ab9d04653
nokdoot <nokdoot@naver.com>
2018-10-03 22:51:34 +0900

    ajax Àü¼Û

--- src/main/java/com/share/controller/SampleController.java
+++ src/main/java/com/share/controller/SampleController.java
@@ -4,16 +4,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class SampleController {
@@ -26,7 +22,7 @@ public class SampleController {
 	
 	
 
-	@GetMapping("/kakaoTest")
+	@GetMapping("kakaoTest")
 	public void kakaoTest(Model model) {
 		
 		System.out.println("!!!!!!!!!!!!!!!!");
@@ -35,23 +31,17 @@ public class SampleController {
 	
 	
 
-	@GetMapping("/kakaoTest2")
+	@GetMapping("kakaoTest2")
 	public void kakaoTest2(Model model) {
-		
-
 		model.addAttribute("greeting","돌아옴");
 	}
 	
 
 	@RequestMapping(value="/kakaoLogin", produces = "application/json", method = {RequestMethod.GET,RequestMethod.POST})
-	public void kakaoLogin(@RequestParam("code") String code, HttpServletRequest request, HttpServletResponse httpservlet)
+	public void kakaoLogin(@RequestBody String code, HttpServletRequest request, HttpServletResponse httpservlet)
 	{
-		
+		System.out.println("asdasd");
 		System.out.println("code : "+code);
 	}
-	
-	
-	
-	
 	
 }
