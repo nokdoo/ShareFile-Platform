6c6383eeadd0f698678627ccf0f3bbd1a0213895
nokdoot <nokdoot@naver.com>
2018-10-05 20:23:01 +0900

    카카오 로그인 인증, 쿠키 생성, 쿠키정보 서버에 저장, rest 호출들(토큰 유효성검사, 토큰갱신, 카카오톡 아이디 정보가져오기), jwt는 아직...

--- src/main/java/com/share/controller/SampleController.java
+++ src/main/java/com/share/controller/SampleController.java
@@ -1,5 +1,9 @@
 package com.share.controller;
 
+import java.io.UnsupportedEncodingException;
+import java.util.Map;
+
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -9,39 +13,64 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import com.share.oauth.RESTcaller;
+import com.share.oauth.Token;
 
 @Controller
 public class SampleController {
 
 	@GetMapping("/sample1")
-	public void sample1(Model model) {
-		
-		model.addAttribute("greeting","안냐세요");
-	}
-	
-	
+	public void sample1(Model model, HttpServletRequest req) {
 
-	@GetMapping("kakaoTest")
+		model.addAttribute("greeting", "안냐세요");
+	}
+
+	@GetMapping("/kakaoTest")
 	public void kakaoTest(Model model) {
-		
+
 		System.out.println("!!!!!!!!!!!!!!!!");
-		model.addAttribute("greeting","안냐세요");
+		model.addAttribute("greeting", "안냐세요");
 	}
-	
-	
 
-	@GetMapping("kakaoTest2")
+	@GetMapping("/kakaoTest2")
 	public void kakaoTest2(Model model) {
-		model.addAttribute("greeting","돌아옴");
+		model.addAttribute("greeting", "돌아옴");
 	}
-	
 
-	@RequestMapping(value="/kakaoLogin", produces = "application/json", method = {RequestMethod.GET,RequestMethod.POST})
-	public void kakaoLogin(@RequestBody String code, HttpServletRequest request, HttpServletResponse httpservlet)
-	{
-		System.out.println("asdasd");
-		System.out.println("code : "+code);
+	/* javascript 로그인 말고 restapi 로그인할 때 필요
+	 * @RequestMapping("/oauth")
+	public String Ouath(Model model, HttpServletRequest req) {
+		model.addAttribute("greeting", "돌아옴");
+		System.out.println("YOUN");
+		return "sample1";
+	}*/
+
+	@RequestMapping("mustLogin")
+	public void mustLogin(Model model, HttpServletRequest req, HttpServletResponse res) throws Exception {
+		model.addAttribute("greeting", "돌아옴");
+		Token token = new Token(req);
+		if(!token.isLogined()) throw new Exception("로그인 하자"); // 로그인 필요
+		Cookie token_cookie = new Cookie("token", token.getAccess_token()+"."+token.getRefreshToken());		
+		res.addCookie(token_cookie);
+		
+		
+		
+		// 그냥 만든 것, 사용자 카카오톡 아이디만 가져옴
+		String user_id = RESTcaller.getUserInfo(token);
+		System.out.println(user_id);
 	}
 	
+	@RequestMapping(value = "/kakaoLogin", produces = "application/json; charset=utf-8", method = { RequestMethod.GET,
+			RequestMethod.POST })
+	public String kakaoLogin(@RequestBody Map<String, String> code, HttpServletRequest req, HttpServletResponse res)
+			throws UnsupportedEncodingException {
+		System.out.println(code);
+		Token token = new Token(code);
+		token.addToken();
+		Cookie token_Cookie = new Cookie("token", token.getAccess_token()+"."+token.getRefreshToken());
+		res.addCookie(token_Cookie);
+		return "mustLogin";
+	}
+
 }
---
+++ src/main/java/com/share/oauth/RESTcaller.java
@@ -0,0 +1,112 @@
+package com.share.oauth;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class RESTcaller {
+	
+	public static boolean checkValidity(Token token) throws IOException {
+		String url = "https://kapi.kakao.com/v1/user/access_token_info";
+		URL obj = new URL(url);
+		HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+		con.setRequestMethod("GET");
+		con.setRequestProperty("Authorization", "Bearer "+token.getAccess_token());
+		con.setRequestProperty("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+		int http_code = con.getResponseCode();
+		if(http_code != 200) {
+			return false;
+		}
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream(), "UTF-8"));
+		StringBuffer sb = new StringBuffer();
+		String line = null;
+        while ((line = br.readLine()) != null) {
+            sb.append(line);
+        }
+        System.out.println(sb);
+        //sb = {"id":930592143,"expiresInMillis":7198373,"appId":232495}
+        //이 정보 필요함?
+		return true;
+	}
+	
+	public static Token updateToken(Token token) throws IOException {
+		String url = "https://kauth.kakao.com/oauth/token";
+		URL obj = new URL(url);
+		HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+		con.setRequestMethod("POST");
+		con.setDoOutput(true);
+		
+		//내 rest api key
+		String client_id = "04508ced113435af63e4b06ce58d2f20";
+		//String client_id = "";
+		String params = "grant_type=refresh_token&client_id="+client_id+"&refresh_token="+token.getRefreshToken();
+		OutputStreamWriter writer = new OutputStreamWriter(con.getOutputStream());
+		writer.write(params);
+		writer.flush();
+		
+		int http_code = con.getResponseCode();
+		
+		Token new_token = null;
+		if(http_code == 200) {
+			BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream(), "UTF-8"));
+			 
+			StringBuffer sb = new StringBuffer();
+			String line = null;
+	        while ((line = br.readLine()) != null) {
+	            sb.append(line);
+	        }
+	        //System.out.println(sb);
+	        JSONObject json_obj = new JSONObject(sb.toString());
+	        Map<String, String> map = new HashMap<>();
+			Iterator<String> iter = json_obj.keys();
+			while(iter.hasNext()) {
+				String key = iter.next();
+				map.put(key, json_obj.get(key).toString());
+			}
+			
+	        new_token = new Token(map);
+	        if(new_token.getRefreshToken() == null)
+				new_token.setRefreshToken(token.getRefreshToken());
+	        new_token.addToken();
+	        token.removeToken();
+		}
+		return new_token;
+		//return tokens;
+	}
+
+	public static String getUserInfo(Token token) throws Exception {
+		String url = "https://kapi.kakao.com/v2/user/me";
+		URL obj = new URL(url);
+		HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+		con.setRequestMethod("GET");
+		con.setRequestProperty("Authorization", "Bearer "+token.getAccess_token());
+
+		int http_code = con.getResponseCode();
+		if(http_code != 200) {
+			throw new Exception();
+		}
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream(), "UTF-8"));
+		StringBuffer sb = new StringBuffer();
+		String line = null;
+        while ((line = br.readLine()) != null) {
+            sb.append(line);
+        }
+        System.out.println(sb);
+        JSONObject json_obj = new JSONObject(sb.toString());
+		return json_obj.get("id").toString();
+	}
+}
