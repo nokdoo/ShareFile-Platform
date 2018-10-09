8e54fa29705f03d138b338fe27453625cd61d13d
nokdoot <nokdoot@naver.com>
2018-10-10 08:56:55 +0900

    upload

--- src/main/java/com/common/controller/CommonController.java
+++ src/main/java/com/common/controller/CommonController.java
@@ -1,30 +1,12 @@
 package com.common.controller;
 
-
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.web.client.TestRestTemplate.HttpClientOption;
 import org.springframework.core.io.ResourceLoader;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -32,17 +14,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.client.RestTemplate;
 
 import com.common.domain.AccountVO;
 import com.common.persistence.AccountRepository;
 import com.common.service.KakaoLogin;
 import com.fasterxml.jackson.databind.JsonNode;
-import com.google.gson.Gson;
-
-import antlr.Token;
-
-
 
 @Controller
 public class CommonController {
@@ -60,13 +36,8 @@ public class CommonController {
 	
 	@GetMapping("/login")
 	public String login(Model model) {
-		
-		
-		
 	
 		//System.out.println(${storageDirectory});
-
-		
 		return "/common/login";
 	}
 	
@@ -91,21 +62,14 @@ public class CommonController {
 		
 	}
 	
-	
-	
-	
 	@GetMapping("/test")
 	public String test(Model model) {
 		
 		AccountVO account = new AccountVO();
 		account.setKakaoID("KAKAO ID!");
 		accountRepo.save(account);
-		
-		
 		return "temp/temp";
 	}
-	
-	
 	
 	@RequestMapping(value="/kakaoLoginTemp", produces = "application/json;", method = {RequestMethod.GET,RequestMethod.POST})
 	public void kakaoLogin(@RequestBody String code, HttpServletRequest request, HttpServletResponse httpservlet)
@@ -114,11 +78,8 @@ public class CommonController {
 		System.out.println("야!!!!!!!!!!!!");
 	}
 	
-	
-
-
-public class token2{
-	int a;
-}
+	public class token2{
+		int a;
+	}
 }
 
