9ddf33e32b2184f1d938e96ed6720546210b664c
HUH SEONGWOOK <chronic794@naver.com>
2018-10-23 19:30:45 +0900

    업로드 기능 수정

--- src/main/java/com/sharefile/service/FileUpload.java
+++ src/main/java/com/sharefile/service/FileUpload.java
@@ -14,11 +14,14 @@ public class FileUpload {
 	
 	public static FileVO storeFile(String storageDirectory, Part part) {
 		FileVO file = new FileVO();
-		String cryptString = Encryptor.GenerateCryptString();
 		
-		file.setName(part.getSubmittedFileName());
-		file.setStoredName(part.getSubmittedFileName());
-		file.setStoredPath(storageDirectory+cryptString);
+		String cryptString = Encryptor.GenerateCryptString();
+		String fileName = part.getSubmittedFileName();
+		String extension = fileName.substring(fileName.lastIndexOf("."));
+		
+		file.setName(fileName);
+		file.setStoredName(cryptString+extension);
+		file.setStoredPath(storageDirectory+cryptString+extension);
 		file.setAccessdate(LocalDateTime.now());
 		file.setRegdate(LocalDateTime.now());
 		
